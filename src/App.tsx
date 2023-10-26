53186e88e1e82fc3c06809aeb17fead6798c4662
farzaank <fkaiyom@gmail.com>
2023-10-26 11:17:27 -0700

    fix fmt

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,20 @@ import Run from "@/routes/Run";
 import Landing from "@/routes/Landing";
 import Leaderboard from "@/routes/Leaderboard";
 export default function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route path={"/"} element={<Landing />} />
-					<Route path="/models" element={<Models />} />
-					<Route path="/leaderboard" element={<Leaderboard />} />
-					<Route path="/scenarios" element={<Scenarios />} />
-					<Route path="/groups" element={<Groups />} />
-					<Route path="/groups/:groupName" element={<Group />} />
-					<Route path="/runs" element={<Runs />} />
-					<Route path="/runs/:runName" element={<Run />} />
-				</Route>
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={"/"} element={<Landing />} />
+          <Route path="/models" element={<Models />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/scenarios" element={<Scenarios />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/groups/:groupName" element={<Group />} />
+          <Route path="/runs" element={<Runs />} />
+          <Route path="/runs/:runName" element={<Run />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
