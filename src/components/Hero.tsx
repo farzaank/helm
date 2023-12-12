5ba24b7b8c71c250455984c008f0cca7d70f57f7
farzaank <fkaiyom@gmail.com>
2023-12-12 00:16:53 -0800

    fixes done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
       {/* Container for Image and Leaderboard */}
       <div
         className="flex flex-col md:flex-col lg:flex-row lg:justify-center"
-        style={{ height: "510px" }} // Reduced height by 10%
+        style={{ height: "525px", transform: "scale(0.88)" }} // Reduced height by 10%
       >
         {/* Image section */}
         <div className="w-full lg:w-1/2 flex justify-center mb-4 lg:mb-0 h-full py-10">
@@ -33,15 +33,15 @@ export default function Hero() {
         {/* Leaderboard section */}
         <div className="w-full lg:w-1/2 flex justify-center h-full py-10">
           <div
-            className="py-2 pb-4 rounded-3xl bg-gray-100 h-full" // Stretched to full height
+            className="py-2 pb-6 rounded-3xl bg-gray-100 h-full" // Stretched to full height
             style={{ maxWidth: "100%" }}
           >
             <MiniLeaderboard></MiniLeaderboard>
             <div className="flex justify-end">
               <Link to="leaderboard">
                 <button
-                  className="px-4 mx-3 btn bg-white rounded-md"
-                  style={{ transform: "scale(0.7)" }}
+                  className="px-4 mx-3 mt-1 btn bg-white rounded-md"
+                  style={{ transform: "scale(0.9)" }}
                 >
                   <span>See More</span>
                 </button>
