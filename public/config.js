b0316933470d70fcfacc5f3041d9df5cf67e3aee
farzaank <fkaiyom@gmail.com>
2024-02-14 23:28:01 -0800

    temporary changes

--- public/config.js
+++ public/config.js
@@ -1,4 +1,6 @@
 window.BENCHMARK_OUTPUT_BASE_URL =
-  "https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/";
+	"https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/";
 window.SUITE = null;
 window.RELEASE = "v1.0.0";
+window.MANIFEST_JSON_URL = null;
+window.MANIFEST_ID = 0;
