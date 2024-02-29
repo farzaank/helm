47c5ef36ff51f1d80f8150bc4ac5f0c8227c22ff
farzaank <fkaiyom@gmail.com>
2024-02-28 21:32:08 -0800

    fixes

--- public/config.js
+++ public/config.js
@@ -2,5 +2,4 @@ window.SUITE = null;
 window.RELEASE = "v1.0.0";
 window.BENCHMARK_OUTPUT_BASE_URL =
 	"https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/";
-window.MANIFEST_JSON_URL = null;
-window.MANIFEST_ID = 0;
+window.RELEASE_INDEX_ID = "lite";
