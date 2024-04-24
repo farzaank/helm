470e5304a83ccbee7fd3fc61471c3eed6d95b123
farzaank <fkaiyom@gmail.com>
2024-04-23 19:49:10 -0700

    catch up

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,23 +3,23 @@ import LiteLanding from "@/components/Landing/LiteLanding";
 import MMLULanding from "@/components/Landing/MMLULanding";
 import HEIMLanding from "@/components/Landing/HEIMLanding";
 import VHELMLanding from "@/components/VHELMLanding";
-import GlobalLanding from "@/components/Landing/GlobalLanding";
+import HomeLanding from "@/components/Landing/HomeLanding";
 
 export default function Home() {
-  // TODO consider a more streamlined way to do this?
-  if (window.PROJECT_ID === "lite") {
-    return <LiteLanding />;
-  } else if (window.PROJECT_ID === "instruct") {
-    return <InstructLanding />;
-  } else if (window.PROJECT_ID === "heim") {
-    return <HEIMLanding />;
-  } else if (window.PROJECT_ID === "mmlu") {
-    return <MMLULanding />;
-  } else if (window.PROJECT_ID === "vhelm") {
-    return <VHELMLanding />;
-  } else if (window.PROJECT_ID === "global") {
-    return <GlobalLanding />;
-  } else {
-    return <LiteLanding />;
-  }
+	// TODO consider a more streamlined way to do this?
+	if (window.PROJECT_ID === "lite") {
+		return <LiteLanding />;
+	} else if (window.PROJECT_ID === "instruct") {
+		return <InstructLanding />;
+	} else if (window.PROJECT_ID === "heim") {
+		return <HEIMLanding />;
+	} else if (window.PROJECT_ID === "mmlu") {
+		return <MMLULanding />;
+	} else if (window.PROJECT_ID === "vhelm") {
+		return <VHELMLanding />;
+	} else if (window.PROJECT_ID === "home") {
+		return <HomeLanding />;
+	} else {
+		return <LiteLanding />;
+	}
 }
