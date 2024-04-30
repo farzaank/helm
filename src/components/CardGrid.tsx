554122fa53be58a4ca8ff13c839f0208ffd56727
farzaank <fkaiyom@gmail.com>
2024-04-30 16:33:53 -0700

    stuff

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -8,7 +8,9 @@ export default function CardGrid() {
   >();
 
   useEffect(() => {
-    fetch("project_metadata.json")
+    fetch(
+      "https://raw.githubusercontent.com/farzaank/helm/main/project_metadata.json",
+    )
       .then((response) => response.json())
       .then((data: ReleaseIndexEntry[]) => {
         setProjectEntries(data);
